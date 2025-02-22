adfede501f34f1fec0c5f45ca1e5b4ecb4f04777
Karys Barbosa <karys.barbosa@outlook.com>
2025-02-22 14:56:19 -0300

    geral com warnings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Cadastro from './pages/Cadastro';
 import Login from './pages/Login';
 import Inicio from './pages/Inicio';
+import Geral from './pages/Geral';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Inicio />} />
           <Route path="/login" element={<Login />} />
           <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/geral" element={<Geral />} />
         </Routes>
       </div>
     </Router>
